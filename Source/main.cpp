0802afa64fccc869309a0cec9606146ece051f16
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-08-24 14:47:12 +0800

    Use the file.hpp functions

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,35 @@
-#include <iostream>
+#include "stdafx.h"
 #include <conio.h>		// _kbhit
+#include <string>
+#include <algorithm>	// std::random_shuffle
+#include <random>		// std::default_random_engine
+
+#include "files.hpp"
+#include "tiff.hpp"
+
+inline void waitForKeypress() {
+	std::cout << "Press any key to continue..." << std::endl;
+	while (!_kbhit());
+}
 
 int main(void) {
-	std::cout << "Hello world!" << std::endl;
+	fs::path p("G:\\dummy_cell");
 
-	std::cout << "Press any key to continue..." << std::endl;
-	while(!_kbhit());
+	std::cout << "Scanning " << p << "... ";
+	std::vector<fs::path> fileList;
+	listTiffFiles(p, fileList);
+	std::cout << fileList.size() << " stack(s) found" << std::endl;
+
+	int rmCnt = sortSpimStacks(fileList);
+	if (rmCnt > 0) {
+		std::cout << "... " << rmCnt << " file(s) removed from the list" << std::endl;
+	}
+
+	for (const fs::path &file : fileList) {
+		dealStack(file);
+	}
+
+	waitForKeypress();
 
 	return 0;
 }
