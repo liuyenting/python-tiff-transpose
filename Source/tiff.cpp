e051e82e78c535a1b3059c8a1cc856ec67cb63bd
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-08-25 10:56:35 +0800

    Refactor the tag selection

--- Source/tiff.cpp
+++ Source/tiff.cpp
@@ -54,6 +54,8 @@ static int cpData(TIFF *in, TIFF *out, uint32_t nrow) {
     if (TIFFGetField(in, tag, &v1, &v2, &v3, &v4)) \
         TIFFSetField(out, tag, v1, v2, v3, v4)
 
+#define CP_BY_PTR (uint16_t)(-1)
+
 static void cpTag(TIFF* in, TIFF* out,
                   uint16_t tag, uint16_t count, TIFFDataType type) {
 	switch (type) {
@@ -67,7 +69,7 @@ static void cpTag(TIFF* in, TIFF* out,
 		} else if (count == 4) {
 			uint16_t *tr, *tg, *tb, *ta;
 			CopyField4(tag, tr, tg, tb, ta);
-		} else if (count == (uint16_t)(-1)) {
+		} else if (count == CP_BY_PTR) {
 			uint16_t shortv1;
 			uint16_t* shortav;
 			CopyField2(tag, shortv1, shortav);
@@ -81,7 +83,7 @@ static void cpTag(TIFF* in, TIFF* out,
 		if (count == 1) {
 			float floatv;
 			CopyField(tag, floatv);
-		} else if (count == (uint16_t)(-1)) {
+		} else if (count == CP_BY_PTR) {
 			float* floatav;
 			CopyField(tag, floatav);
 		}
@@ -94,7 +96,7 @@ static void cpTag(TIFF* in, TIFF* out,
 		if (count == 1) {
 			double doublev;
 			CopyField(tag, doublev);
-		} else if (count == (uint16_t)(-1)) {
+		} else if (count == CP_BY_PTR) {
 			double* doubleav;
 			CopyField(tag, doubleav);
 		}
@@ -106,62 +108,78 @@ static void cpTag(TIFF* in, TIFF* out,
 }
 #define	CopyTag(tag, count, type) cpTag(in, out, tag, count, type)
 
-static struct cpTag {
+static struct defTagList {
 	uint16_t tag;
 	uint16_t count;
 	TIFFDataType type;
 } tags[] = {
-	{ TIFFTAG_SUBFILETYPE,		1, TIFF_LONG  },
-	{ TIFFTAG_THRESHHOLDING,	1, TIFF_SHORT },
-	{ TIFFTAG_DOCUMENTNAME,		1, TIFF_ASCII },
-	{ TIFFTAG_IMAGEDESCRIPTION,	1, TIFF_ASCII },
+    { TIFFTAG_ARTIST,		    1, TIFF_ASCII },
+    { TIFFTAG_BITSPERSAMPLE,    1, TIFF_SHORT },
+    { TIFFTAG_COLORMAP,         4, TIFF_SHORT },
+    { TIFFTAG_COMPRESSION,      1, TIFF_SHORT },
+    { TIFFTAG_DATETIME,		    1, TIFF_ASCII },
+    { TIFFTAG_DOCUMENTNAME,		1, TIFF_ASCII },
+    { TIFFTAG_DOTRANGE,		    2, TIFF_SHORT },
+    { TIFFTAG_EXTRASAMPLES,	CP_BY_PTR, TIFF_SHORT },
+    { TIFFTAG_FILLORDER,        1, TIFF_SHORT },
+    { TIFFTAG_HALFTONEHINTS,    2, TIFF_SHORT },
+    { TIFFTAG_HOSTCOMPUTER,		1, TIFF_ASCII },
+    { TIFFTAG_IMAGEDESCRIPTION,	1, TIFF_ASCII },
 	{ TIFFTAG_MAKE,			    1, TIFF_ASCII },
+    { TIFFTAG_MAXSAMPLEVALUE,	1, TIFF_SHORT },
+    { TIFFTAG_MINSAMPLEVALUE,	1, TIFF_SHORT },
 	{ TIFFTAG_MODEL,		    1, TIFF_ASCII },
-	{ TIFFTAG_MINSAMPLEVALUE,	1, TIFF_SHORT },
-	{ TIFFTAG_MAXSAMPLEVALUE,	1, TIFF_SHORT },
-	{ TIFFTAG_XRESOLUTION,		1, TIFF_RATIONAL },
+    { TIFFTAG_PAGENAME,		    1, TIFF_ASCII },
+    { TIFFTAG_PHOTOMETRIC,      1, TIFF_SHORT },
+    { TIFFTAG_PLANARCONFIG,     1, TIFF_SHORT },
+    { TIFFTAG_PRIMARYCHROMATICITIES, CP_BY_PTR, TIFF_RATIONAL },
+    { TIFFTAG_REFERENCEBLACKWHITE, CP_BY_PTR, TIFF_RATIONAL },
+    { TIFFTAG_RESOLUTIONUNIT,   1, TIFF_SHORT },
+    { TIFFTAG_SAMPLEFORMAT,	    1, TIFF_SHORT },
+    { TIFFTAG_SMAXSAMPLEVALUE,	1, TIFF_DOUBLE },
+    { TIFFTAG_SMINSAMPLEVALUE,	1, TIFF_DOUBLE },
+	{ TIFFTAG_SOFTWARE,		    1, TIFF_ASCII },
+    { TIFFTAG_STONITS,		    1, TIFF_DOUBLE },
+    { TIFFTAG_SUBFILETYPE,		1, TIFF_LONG  },
+    { TIFFTAG_TARGETPRINTER,    1, TIFF_ASCII },
+	{ TIFFTAG_THRESHHOLDING,	1, TIFF_SHORT },
+    { TIFFTAG_WHITEPOINT, CP_BY_PTR, TIFF_RATIONAL },
+    { TIFFTAG_XPOSITION,		1, TIFF_RATIONAL },
+    { TIFFTAG_XRESOLUTION,		1, TIFF_RATIONAL },
+	{ TIFFTAG_YPOSITION,	    1, TIFF_RATIONAL },
 	{ TIFFTAG_YRESOLUTION,		1, TIFF_RATIONAL },
-	{ TIFFTAG_PAGENAME,		    1, TIFF_ASCII },
-	{ TIFFTAG_XPOSITION,		1, TIFF_RATIONAL },
-	{ TIFFTAG_YPOSITION,		1, TIFF_RATIONAL },
-	{ TIFFTAG_RESOLUTIONUNIT,	1, TIFF_SHORT },
-	{ TIFFTAG_SOFTWARE,		1, TIFF_ASCII },
-	{ TIFFTAG_DATETIME,		1, TIFF_ASCII },
-	{ TIFFTAG_ARTIST,		1, TIFF_ASCII },
-	{ TIFFTAG_HOSTCOMPUTER,		1, TIFF_ASCII },
-	{ TIFFTAG_WHITEPOINT,		(uint16_t) -1, TIFF_RATIONAL },
-	{ TIFFTAG_PRIMARYCHROMATICITIES,(uint16_t) -1,TIFF_RATIONAL },
-	{ TIFFTAG_HALFTONEHINTS,	2, TIFF_SHORT },
-	{ TIFFTAG_INKSET,		1, TIFF_SHORT },
-	{ TIFFTAG_DOTRANGE,		2, TIFF_SHORT },
-	{ TIFFTAG_TARGETPRINTER,	1, TIFF_ASCII },
-	{ TIFFTAG_SAMPLEFORMAT,		1, TIFF_SHORT },
-	{ TIFFTAG_REFERENCEBLACKWHITE,	(uint16_t)(-1),TIFF_RATIONAL },
-	{ TIFFTAG_EXTRASAMPLES,		(uint16_t)(-1), TIFF_SHORT },
-	{ TIFFTAG_SMINSAMPLEVALUE,	1, TIFF_DOUBLE },
-	{ TIFFTAG_SMAXSAMPLEVALUE,	1, TIFF_DOUBLE },
-	{ TIFFTAG_STONITS,		1, TIFF_DOUBLE },
 };
 #define	NTAGS (sizeof(tags) / sizeof(tags[0]))
 
 static int cpTiff(TIFF* in, TIFF* out,
                   const uint16_t iLayer, const uint16_t nLayer)
 {
-	uint16_t bitspersample, samplesperpixel;
+	uint16_t samplesperpixel;
 	uint16_t orientation;
 	uint32_t width, length, rowsperstrip;
-	struct cpTag* p;
 
+    // Image size and memory layout.
 	CopyField(TIFFTAG_IMAGEWIDTH, width);
 	CopyField(TIFFTAG_IMAGELENGTH, length);
-	CopyField(TIFFTAG_BITSPERSAMPLE, bitspersample);
-	CopyField(TIFFTAG_SAMPLESPERPIXEL, samplesperpixel);
-    CopyTag(TIFFTAG_COMPRESSION, 1, TIFF_SHORT);
-	CopyTag(TIFFTAG_PHOTOMETRIC, 1, TIFF_SHORT);
-	CopyTag(TIFFTAG_FILLORDER, 1, TIFF_SHORT);
-	/*
-	 * Will copy `Orientation' tag from input image
-	 */
+    {
+		/*
+		 * RowsPerStrip is left unspecified: use either the
+		 * value from the input image or, if nothing is defined,
+		 * use the library default.
+		 */
+		if (!TIFFGetField(in, TIFFTAG_ROWSPERSTRIP, &rowsperstrip)) {
+			rowsperstrip = TIFFDefaultStripSize(out, rowsperstrip);
+		}
+		if (rowsperstrip > length && rowsperstrip != (uint32_t)-1) {
+			rowsperstrip = length;
+        }
+		TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, rowsperstrip);
+	}
+    CopyField(TIFFTAG_SAMPLESPERPIXEL, samplesperpixel);
+	if (samplesperpixel <= 4)
+		CopyTag(TIFFTAG_TRANSFERFUNCTION, 4, TIFF_SHORT);
+
+    // Image orientation (per page).
 	TIFFGetFieldDefaulted(in, TIFFTAG_ORIENTATION, &orientation);
 	switch (orientation) {
 		case ORIENTATION_BOTRIGHT:
@@ -183,24 +201,8 @@ static int cpTiff(TIFF* in, TIFF* out,
 			break;
 	}
 	TIFFSetField(out, TIFFTAG_ORIENTATION, orientation);
-	{
-		/*
-		 * RowsPerStrip is left unspecified: use either the
-		 * value from the input image or, if nothing is defined,
-		 * use the library default.
-		 */
-		if (!TIFFGetField(in, TIFFTAG_ROWSPERSTRIP, &rowsperstrip)) {
-			rowsperstrip = TIFFDefaultStripSize(out, rowsperstrip);
-		}
-		if (rowsperstrip > length && rowsperstrip != (uint32_t)-1) {
-			rowsperstrip = length;
-        }
-		TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, rowsperstrip);
-	}
-    CopyTag(TIFFTAG_PLANARCONFIG, 1, TIFF_SHORT);
-	if (samplesperpixel <= 4)
-		CopyTag(TIFFTAG_TRANSFERFUNCTION, 4, TIFF_SHORT);
-	CopyTag(TIFFTAG_COLORMAP, 4, TIFF_SHORT);
+
+    // Colormap profile.
 	{
 		uint32_t len32;
 		void** data;
@@ -225,11 +227,13 @@ static int cpTiff(TIFF* in, TIFF* out,
 			}
 		}
 	}
-    // Set the page number.
+
+    // Page number.
 	TIFFSetField(out, TIFFTAG_PAGENUMBER, iLayer, nLayer);
 
-	for (p = tags; p < &tags[NTAGS]; p++)
-		CopyTag(p->tag, p->count, p->type);
+    // Rest of the tags, directly copy them. 
+	for (struct defTagList *t = tags; t < &tags[NTAGS]; t++)
+		CopyTag(t->tag, t->count,t->type);
 
 	return cpData(in, out, length);
 }
